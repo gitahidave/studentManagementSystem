6d412e237a70d73cc95f6c2053b2fee9992a76e7
David Gitahi <gitahidavid07@gmail.com>
2026-09-24 19:06:08 +0300

    Add Student model, controller methods, and migration for students table

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+
 class StudentController extends Controller
 {
     public function index()
     {
-        return view('students.hello');
+        $students = Student::all();
+
+        return view('students.hello', compact('students'));
+    }
+
+    public function show(Student $student)
+    {
+        return view('students.hello', compact('student'));
     }
 }
