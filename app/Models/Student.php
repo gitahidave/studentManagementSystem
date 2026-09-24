3b968f91d269a81c1a358034c6d68e99f89edb97
David Gitahi <gitahidavid07@gmail.com>
2026-09-24 21:13:57 +0300

    Add columns to students table migration for firstname, secondname, email, phoneno, and course

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable=[
+        'firstname',
+        'secondname',
+        'email',
+        'phoneno',
+        'course'
+    ];
 }
--- database/migrations/2026_09_24_174342_create_students_table.php
+++ database/migrations/2026_09_24_174342_create_students_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname');
+            $table->string('secondname');
+            $table->string('email')->unique();
+            $table->string('phoneno');
+            $table->string('course');
             $table->timestamps();
         });
     }
